13b83093c4e7c95b31d4c2d8831bdf74c36044e4
Denis Belov <dsbelov63@gmail.com>
2022-06-29 09:09:22 +0400

    added root dir

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = function WebpackLoader(source)
 {
 	const loader_options = this.loaders?.[this.loaderIndex]?.options || {};
 
+	loader_options.root = loader_options.root || '';
+
 	const source_options = JSON.parse(source);
 
 	const options =
@@ -22,13 +24,13 @@ module.exports = function WebpackLoader(source)
 
 
 
-	options.watchFiles.forEach((elm) => this.addDependency(elm));
+	options.watchFiles.forEach((elm) => this.addDependency(path.join(loader_options.root, elm)));
 
-	options.watchDirectories.forEach((elm) => this.addContextDependency(elm));
+	options.watchDirectories.forEach((elm) => this.addContextDependency(path.join(loader_options.root, elm)));
 
 	if (options.execute)
 	{
-		console.log(execSync(`${ options.execute }`, { encoding: 'utf8' }));
+		console.log(execSync(`${ options.execute.replace('$(ROOT)', loader_options.root) }`, { encoding: 'utf8' }));
 	}
 
 
@@ -36,7 +38,7 @@ module.exports = function WebpackLoader(source)
 	const buffer =
 		Array.prototype.slice.call
 		(
-			fs.readFileSync(path.resolve(options.target)),
+			fs.readFileSync(path.resolve(path.join(loader_options.root, options.target))),
 		);
 
 	const result = `/*eslint-disable*/ export default new Uint8Array([ ${ buffer } ]).buffer;`;
